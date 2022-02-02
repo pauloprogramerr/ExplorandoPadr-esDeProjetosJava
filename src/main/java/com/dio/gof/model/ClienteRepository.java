164d97f4ec58e2fa33d811684a8cd13086855001
pauloprogramerr <47704075+pauloprogramerr@users.noreply.github.com>
2022-02-02 15:24:28 -0300

    Atualização de arquivos do projeto, ainda em andamento.

--- src/main/java/com/dio/gof/model/ClienteRepository.java
+++ src/main/java/com/dio/gof/model/ClienteRepository.java
@@ -2,9 +2,8 @@ package com.dio.gof.model;
 
 
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
 
 
-@Repository
 public interface ClienteRepository extends CrudRepository<Cliente, Long> {
+
 }
--- src/main/java/com/dio/gof/model/service/ClienteService.java
+++ src/main/java/com/dio/gof/model/service/ClienteService.java
@@ -2,6 +2,13 @@ package com.dio.gof.model.service;
 
 import com.dio.gof.model.Cliente;
 
+/**
+ * Interface que define o padrão <b>Strategy</b> no domínio de cliente com
+ * isso, se necessário, podemos ter multiplas implementações dessa mesma interface.
+ *
+ * @author PauloProgram
+ */
+
 public interface ClienteService {
     Iterable<Cliente> buscarTodos();
 
