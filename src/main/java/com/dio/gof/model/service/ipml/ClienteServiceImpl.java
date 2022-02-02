e2b336b6202aaaf6857c68694f998cebb1adba2c
pauloprogramerr <47704075+pauloprogramerr@users.noreply.github.com>
2022-02-02 15:27:51 -0300

    Atualização de arquivos do projeto, ainda em andamento.

---
+++ src/main/java/com/dio/gof/model/service/ipml/ClienteServiceImpl.java
@@ -0,0 +1,39 @@
+package com.dio.gof.model.service.ipml;
+
+import com.dio.gof.model.Cliente;
+import com.dio.gof.model.service.ClienteService;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ */
+
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
